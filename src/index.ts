512bd7363eee367d17b8aff4534ee2b5cfe7b25b
Korede Anunusor <koredeanunusor@gmai.com>
2025-01-17 15:54:34 +0100

    build new project

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import express from "express";
+import { errorHandler } from "./utils/errorHandler.util";
+
+dotenv.config();
+
+const portEnv = process.env.PORT;
+if(!portEnv){
+    console.error("Error: PORT is not defined in .env file");
+    process.exit(1);
+}
+
+const PORT:number = parseInt(portEnv, 10);
+if(isNaN(PORT)){
+    console.error("Error: PORT is not a number in .env file");
+    process.exit(1);
+}
+
+const app = express();
+const corsOptions = {
+    origin:
+    "*",
+    credentials: true,
+    allowedHeaders: "*",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+};
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    
+});
